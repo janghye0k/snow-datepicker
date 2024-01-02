35a623c9bdb9847b61cd3177f32174bef5f32ff7
JangHyeok Kim <d0or.hyeok@gmail.com>
2024-01-02 09:00:30 +0900

    feat: create util

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 /**
  * @param {string} title
  * @param {string} [body]
@@ -7,3 +9,54 @@ export function error(title, body) {
   if (typeof body === 'string') messages.push(body);
   throw new Error(messages.join('\n'));
 }
+
+/**
+ * @param {any} value
+ * @returns {boolean}
+ */
+export function isDate(value) {
+  if (dayjs.isDayjs(value)) return true;
+  return !isNaN(/** @type {any} */ (new Date(value)));
+}
+
+/**
+ * @param {unknown} value
+ * @returns {value is string}
+ */
+export const isString = (value) => typeof value === 'string';
+
+/**
+ * @param {unknown} value
+ * @returns {value is undefined}
+ */
+export const isUndefined = (value) => value === undefined;
+
+/**
+ * @param {unknown} value
+ * @returns {value is null}
+ */
+export const isNull = (value) => value === null;
+
+/**
+ * @param {unknown} value
+ * @returns {value is number}
+ */
+export const isNumber = (value) => typeof value === 'number';
+
+/**
+ * @param {unknown} value
+ * @returns {value is boolean}
+ */
+export const isBoolean = (value) => typeof value === 'boolean';
+
+/**
+ * @param {unknown} value
+ * @returns {value is Function}
+ */
+export const isFunction = (value) => typeof value === 'function';
+
+/**
+ * @param {unknown} value
+ * @returns {value is Record<string|number|symbol, any>}
+ */
+export const isObject = (value) => !isNull(value) && typeof value === 'object';
