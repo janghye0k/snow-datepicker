14200b2bf65a1ee022cb44e9450b7d91a9c48ce0
JangHyeok Kim <d0or.hyeok@gmail.com>
2024-02-08 11:58:10 +0900

    feat: dev content

--- src/components/Content.ts
+++ src/components/Content.ts
@@ -1,18 +1,89 @@
 import { cn } from '@/helpers/selectors';
 import Components from './Components';
+import { effect } from '@janghye0k/observable';
+import { create$, isNull } from 'doumi';
+import { parseDate } from '@/helpers/util';
 
 class Content extends Components {
-  render(): void {
-    this.$el.innerHTML = /*html*/ `
-      <div class="${cn('content')}">
-        <div class="${cn('days')}">
-          <div class="${cn('days.weekdays')}"></div>
-          <div class="${cn('days.views')}"></div>
-        </div>
-        <div class="${cn('months')}"></div>
-        <div class="${cn('years')}"></div>
-      </div>
-    `;
+  subscribe(): void {
+    const { store } = this.instance;
+    const unsub = effect(
+      (state) => {
+        this.$el.className = [cn('content'), `--${state}`].join(' ');
+      },
+      [store.state.currentUnit, true]
+    );
+
+    this.unsubscribers.push(unsub);
+  }
+
+  generateWeekDays() {
+    const {
+      locale: { weekdaysMin },
+      weekIndexes,
+    } = this.instance.converter;
+    weekIndexes.map((index: number) =>
+      create$('div', {
+        className: cn('weekday'),
+        dataset: { dayindex: index },
+        innerHTML: weekdaysMin[index],
+      })
+    );
+  }
+
+  generateDays() {
+    const { store, converter } = this.instance;
+    const { unitDate, selectedDate } = store;
+    const [year, month] = parseDate(unitDate);
+
+    const firstDate = new Date(year, month, 1);
+    const lastDate = new Date(year, month + 1, 0);
+    const firstDateDay = firstDate.getDay();
+    const lastDateDay = lastDate.getDay();
+
+    const { weekStart = 0 } = converter.locale;
+    const preDays =
+      (firstDateDay + 7 - weekStart) % 7 || (lastDate.getDate() === 28 ? 7 : 0);
+    const postDays = 6 - lastDateDay;
+
+    const days: { d: number; m: number }[] = [];
+    const displayPrevStart = new Date(year, month, 0).getDate() - preDays + 1;
+
+    const pushData = (length: number, m: number, adder: number = 1) =>
+      Array.from({ length }, (_, i) => days.push({ d: adder + i, m }));
+    pushData(preDays, month - 1, displayPrevStart);
+    pushData(lastDate.getDate(), month);
+    pushData(postDays, (month + 1) % 12);
+
+    const selected = { month: -1, day: -1 };
+    if (!isNull(selectedDate)) {
+      selected.day = selectedDate.getDate();
+      selected.month = selectedDate.getMonth();
+    }
+    const todays = parseDate();
+
+    // Render days
+    days.forEach(({ d, m }) => {
+      const isOtherMonth = m !== month;
+      const isSelectable = !isOtherMonth || this.options.selectOtherMonths;
+      const isHidden = isOtherMonth && !this.options.showOtherMonths;
+
+      const classList = [cn('cell')];
+      if (isOtherMonth) classList.push('other');
+      if (selected.day === d && selected.month === m) classList.push('active');
+      if (todays[0] === year && todays[1] === m && todays[2] === d)
+        classList.push('today');
+
+      const $day = create$('div', {
+        classList,
+        dataset: { year, monthindex: m, day: d },
+        innerHTML: `${d}`,
+      });
+      if (isHidden) $day.setAttribute('hidden', '');
+
+      this.$el.appendChild($day);
+      if (isSelectable) false; // bindevents
+    });
   }
 }
 
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,4 +1,4 @@
-import { isDate } from 'doumi';
+import { isDate, range } from 'doumi';
 
 export function error(title: string, body?: string) {
   const messages = [`[DatePicker]: ${title}`];
@@ -20,4 +20,29 @@ export const decade = (date: Date) => {
   const start = yearPerDecade * 10;
   const end = start + 9;
   return [start, end];
+};
+
+export const isSameDate = (date: Date, compare: Date) => {
+  const arr1 = parseDate(date);
+  const arr2 = parseDate(compare);
+  return arr1.every((value, index) => arr2[index] === value);
+};
+
+export const getCalendarDates = (date: Date, weekStart: number = 0) => {
+  const [year, month] = parseDate(date);
+
+  const lastDate = new Date(year, month + 1, 0);
+  const firstDayIndex = new Date(year, month, 1).getDay();
+  const lastDayIndex = lastDate.getDay();
+
+  const preDaysCount =
+    (firstDayIndex + 7 - weekStart) % 7 || (lastDate.getDate() === 28 ? 7 : 0);
+  const postDaysCount = 6 - lastDayIndex;
+
+  const totalDaysCount = preDaysCount + postDaysCount + lastDate.getDate();
+
+  const results = range(-preDaysCount + 1, totalDaysCount).map(
+    (dayindex) => new Date(year, month, dayindex, 12)
+  );
+  return results;
 };
