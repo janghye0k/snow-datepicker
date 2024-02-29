19f47a500f547fd4192250c49cef0e4f14e62f7b
JangHyeok Kim <d0or.hyeok@gmail.com>
2024-02-29 14:40:24 +0900

    test: add all options & shortcuts test

--- tests/options.test.js
+++ tests/options.test.js
@@ -22,156 +22,11 @@ beforeEach(() => {
 });
 
 describe('OPTIONS TEST', () => {
-  describe('options.className', () => {
-    it('should append custom className to datepicker', () => {
-      const className = 'this-is-custom-class';
-      create($el, { className });
-      expect(datepicker.$datepicker.classList.contains(className)).toBe(true);
-    });
-  });
-
-  describe('options.locale', () => {
-    it('should change locale to ko', async () => {
-      create($el, { locale: ko });
-
-      const weekdaysShort = [];
-      datepicker.$datepicker
-        .querySelectorAll('.hye0k-datepicker-weekday')
-        .forEach(($el) => weekdaysShort.push($el.textContent.trim()));
-
-      expect(
-        weekdaysShort.every((item) => ko.weekdaysShort.includes(item))
-      ).toBe(true);
-    });
-  });
-
-  describe('options.selectedDate', () => {
-    it('should select date on init', async () => {
-      const date = new Date('2024-01-01');
-      create($el, { selectedDate: date });
-
-      const inputDate = new Date(datepicker.$input.value);
-      expect(datepicker.selectedDate).not.toBe(null);
-      expect(date.getFullYear()).toBe(inputDate.getFullYear());
-      expect(date.getMonth()).toBe(inputDate.getMonth());
-      expect(date.getDate()).toBe(inputDate.getDate());
-    });
-  });
-
-  describe('options.view', () => {
-    const date = new Date('2024-01-01');
-    const cases = [
-      { view: 'days', contains: 'January' },
-      { view: 'months', contains: '2024' },
-      { view: 'years', contains: '2020 - 2029' },
-    ];
-    cases.forEach(({ view, contains }) =>
-      it(`should ${view} view`, () => {
-        create($el, { view, selectedDate: date });
-        const $title = datepicker.$datepicker.querySelector(
-          '.' + cn('controlsTitle')
-        );
-        expect($title.textContent.includes(contains)).toBe(true);
-      })
-    );
-  });
-
-  describe('options.showOtherMonths', () => {
-    const date = new Date('2024-01-01');
-
-    it(`other months's day should be visible when pass options true`, () => {
-      create($el, { selectedDate: date, showOtherMonths: true });
+  describe('options.animation', () => {
+    it('calendar element should be reveal with animation', async () => {
+      create($el, { animation: true });
       datepicker.show();
-
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      expect($febFirst).toBeVisible();
-    });
-
-    it(`other months's day should be hidden when pass options false`, () => {
-      create($el, { selectedDate: date, showOtherMonths: false });
-      datepicker.show();
-
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      expect($febFirst.classList.contains('--hidden')).toBe(true);
-    });
-  });
-
-  describe('options.selectOtherMonths', () => {
-    const date = new Date('2024-01-01');
-    it(`should be active when click other month's day`, () => {
-      create($el, { selectedDate: date, selectOtherMonths: true });
-      datepicker.show();
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      $febFirst.click();
-      const selectedDate = datepicker.selectedDate;
-      const arr = [
-        selectedDate.getFullYear(),
-        selectedDate.getMonth() + 1,
-        selectedDate.getDate(),
-      ];
-      expect(arr.join('-')).toBe('2024-2-1');
-    });
-
-    it(`should not be active when click other month's day`, () => {
-      create($el, { selectedDate: date, selectOtherMonths: false });
-      datepicker.show();
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      $febFirst.click();
-      const selectedDate = datepicker.selectedDate;
-      const arr = [
-        selectedDate.getFullYear(),
-        selectedDate.getMonth(),
-        selectedDate.getDate(),
-      ];
-      expect(arr.join('-')).toBe('2024-1-1');
-    });
-  });
-
-  describe('options.moveOtherMonths', () => {
-    const date = new Date('2024-01-01');
-    it(`should be show other month when click other months's day`, () => {
-      create($el, { selectedDate: date, moveOtherMonths: true });
-      datepicker.show();
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      $febFirst.click();
-
-      expect(datepicker.viewDate.getMonth()).toBe(1);
-    });
-
-    it(`should not be show other month when click other months's day`, () => {
-      create($el, { selectedDate: date, moveOtherMonths: false });
-      datepicker.show();
-      const $febFirst = datepicker.$datepicker.querySelector(
-        '[data-monthindex="1"][data-day="1"]'
-      );
-      $febFirst.click();
-
-      expect(datepicker.viewDate.getMonth()).toBe(0);
-    });
-  });
-
-  describe('options.toggleSelected', () => {
-    it('should be unselected when click selected item', () => {
-      create($el, {
-        selectedDate: new Date('2024-01-01'),
-        toggleSelected: true,
-      });
-      datepicker.show();
-      const $day = datepicker.$datepicker.querySelector(
-        '[data-monthindex="0"][data-day="1"]'
-      );
-      $day.click();
-      expect(datepicker.selectedDate).toBe(null);
+      expect(datepicker.$datepicker.classList.contains('--scaleUp')).toBe(true);
     });
   });
 
@@ -194,98 +49,15 @@ describe('OPTIONS TEST', () => {
     });
   });
 
-  it('options.dateFormat', () => {
-    create($el, {
-      selectedDate: new Date('2024-01-01'),
-      dateFormat: 'MM/DD/YYYY',
-    });
-    expect(datepicker.$input.value).toBe('01/01/2024');
-  });
-
-  describe('options.minDate', () => {
-    it('should not be move under minimun date', () => {
+  describe('options.backdrop', () => {
+    it('calendar should be reveal at center position', () => {
       create($el, {
-        selectedDate: new Date('2024-01-01'),
-        minDate: new Date('2024-01-01'),
-        navigationLoop: false,
-      });
-
-      datepicker.prev();
-      expect(datepicker.viewDate.getMonth()).toBe(0);
-    });
-
-    it('should not be select under minimun date', () => {
-      create($el, {
-        selectedDate: new Date('2024-02-01'),
-        minDate: new Date('2024-02-01'),
+        animation: false,
+        backdrop: true,
       });
 
       datepicker.show();
-      const $cell = datepicker.$datepicker.querySelector(
-        '[data-monthindex="0"][data-day="31"]'
-      );
-      $cell.click();
-
-      expect(datepicker.selectedDate.getDate()).not.toBe(31);
-    });
-  });
-
-  describe('options.maxDate', () => {
-    it('should not be move over maximum date', () => {
-      create($el, {
-        selectedDate: new Date('2024-01-01'),
-        maxDate: new Date('2024-01-01'),
-        navigationLoop: false,
-      });
-
-      datepicker.next();
-      expect(datepicker.viewDate.getFullYear()).toBe(2024);
-      expect(datepicker.viewDate.getMonth()).toBe(0);
-      expect(datepicker.viewDate.getDate()).toBe(1);
-    });
-
-    it('should not be select over maximum date', () => {
-      create($el, {
-        selectedDate: new Date('2024-01-01'),
-        maxDate: new Date('2024-01-01'),
-      });
-
-      datepicker.show();
-      const $cell = datepicker.$datepicker.querySelector(
-        '[data-monthindex="0"][data-day="2"]'
-      );
-      $cell.click();
-      expect(datepicker.selectedDate.getDate()).not.toBe(2);
-    });
-  });
-
-  describe('options.navigationLoop', () => {
-    it('should be loop, if `true`', () => {
-      create($el, {
-        selectedDate: new Date('2024-02-01'),
-        navigationLoop: true,
-        minDate: new Date('2024-01-01'),
-        maxDate: new Date('2024-03-01'),
-      });
-
-      datepicker.next();
-      datepicker.next();
-
-      expect(datepicker.viewDate.getMonth()).toBe(0);
-    });
-
-    it('should not be loop, if `false`', () => {
-      create($el, {
-        selectedDate: new Date('2024-02-01'),
-        navigationLoop: false,
-        minDate: new Date('2024-01-01'),
-        maxDate: new Date('2024-03-01'),
-      });
-
-      datepicker.next();
-      datepicker.next();
-
-      expect(datepicker.viewDate.getMonth()).toBe(2);
+      expect(datepicker.$datepicker.classList.contains('--center'));
     });
   });
 
@@ -330,6 +102,341 @@ describe('OPTIONS TEST', () => {
       expect($btn.dataset.test).toBe('test');
       $btn.click();
       expect(isClick).toBe(true);
+    });
+  });
+
+  describe('options.className', () => {
+    it('should append custom className to datepicker', () => {
+      const className = 'this-is-custom-class';
+      create($el, { className });
+      expect(datepicker.$datepicker.classList.contains(className)).toBe(true);
+    });
+  });
+
+  describe('options.dateFormat', () => {
+    it('datepicker input value should be match with dateFormat', () => {
+      create($el, {
+        selectedDate: new Date('2024-01-01'),
+        dateFormat: 'MM/DD/YYYY',
+      });
+      expect(datepicker.$input.value).toBe('01/01/2024');
+    });
+  });
+
+  describe('options.inline', () => {
+    it('calendar should be placed in target element', () => {
+      create($el, { inline: true });
+      expect(datepicker.$target.contains(datepicker.$datepicker)).toBe(true);
+    });
+  });
+
+  describe('options.locale', () => {
+    it('should change locale to ko', async () => {
+      create($el, { locale: ko });
+
+      const weekdaysShort = [];
+      datepicker.$datepicker
+        .querySelectorAll('.hye0k-datepicker-weekday')
+        .forEach(($el) => weekdaysShort.push($el.textContent.trim()));
+
+      expect(
+        weekdaysShort.every((item) => ko.weekdaysShort.includes(item))
+      ).toBe(true);
+    });
+  });
+
+  describe('options.maxDate', () => {
+    it('should not be move over maximum date', () => {
+      create($el, {
+        selectedDate: new Date('2024-01-01'),
+        maxDate: new Date('2024-01-01'),
+        navigationLoop: false,
+      });
+
+      datepicker.next();
+      expect(datepicker.viewDate.getFullYear()).toBe(2024);
+      expect(datepicker.viewDate.getMonth()).toBe(0);
+      expect(datepicker.viewDate.getDate()).toBe(1);
+    });
+
+    it('should not be select over maximum date', () => {
+      create($el, {
+        selectedDate: new Date('2024-01-01'),
+        maxDate: new Date('2024-01-01'),
+      });
+
+      datepicker.show();
+      const $cell = datepicker.$datepicker.querySelector(
+        '[data-monthindex="0"][data-day="2"]'
+      );
+      $cell.click();
+      expect(datepicker.selectedDate.getDate()).not.toBe(2);
+    });
+  });
+
+  describe('options.minDate', () => {
+    it('should not be move under minimun date', () => {
+      create($el, {
+        selectedDate: new Date('2024-01-01'),
+        minDate: new Date('2024-01-01'),
+        navigationLoop: false,
+      });
+
+      datepicker.prev();
+      expect(datepicker.viewDate.getMonth()).toBe(0);
+    });
+
+    it('should not be select under minimun date', () => {
+      create($el, {
+        selectedDate: new Date('2024-02-01'),
+        minDate: new Date('2024-02-01'),
+      });
+
+      datepicker.show();
+      const $cell = datepicker.$datepicker.querySelector(
+        '[data-monthindex="0"][data-day="31"]'
+      );
+      $cell.click();
+
+      expect(datepicker.selectedDate.getDate()).not.toBe(31);
+    });
+  });
+
+  describe('options.moveOtherMonths', () => {
+    const date = new Date('2024-01-01');
+    it(`should be show other month when click other months's day`, () => {
+      create($el, { selectedDate: date, moveOtherMonths: true });
+      datepicker.show();
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      $febFirst.click();
+
+      expect(datepicker.viewDate.getMonth()).toBe(1);
+    });
+
+    it(`should not be show other month when click other months's day`, () => {
+      create($el, { selectedDate: date, moveOtherMonths: false });
+      datepicker.show();
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      $febFirst.click();
+
+      expect(datepicker.viewDate.getMonth()).toBe(0);
+    });
+  });
+
+  describe('options.navigationLoop', () => {
+    it('should be loop, if `true`', () => {
+      create($el, {
+        selectedDate: new Date('2024-02-01'),
+        navigationLoop: true,
+        minDate: new Date('2024-01-01'),
+        maxDate: new Date('2024-03-01'),
+      });
+
+      datepicker.next();
+      datepicker.next();
+
+      expect(datepicker.viewDate.getMonth()).toBe(0);
+    });
+
+    it('should not be loop, if `false`', () => {
+      create($el, {
+        selectedDate: new Date('2024-02-01'),
+        navigationLoop: false,
+        minDate: new Date('2024-01-01'),
+        maxDate: new Date('2024-03-01'),
+      });
+
+      datepicker.next();
+      datepicker.next();
+
+      expect(datepicker.viewDate.getMonth()).toBe(2);
+    });
+  });
+
+  describe('options.placeHolder', () => {
+    it('should be change input placeholder', () => {
+      const text = 'my-text';
+      create($el, { placeHolder: text });
+      expect(datepicker.$input.placeholder).toBe(text);
+    });
+  });
+
+  describe('options.selectOtherMonths', () => {
+    const date = new Date('2024-01-01');
+    it(`should be active when click other month's day`, () => {
+      create($el, { selectedDate: date, selectOtherMonths: true });
+      datepicker.show();
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      $febFirst.click();
+      const selectedDate = datepicker.selectedDate;
+      const arr = [
+        selectedDate.getFullYear(),
+        selectedDate.getMonth() + 1,
+        selectedDate.getDate(),
+      ];
+      expect(arr.join('-')).toBe('2024-2-1');
+    });
+
+    it(`should not be active when click other month's day`, () => {
+      create($el, { selectedDate: date, selectOtherMonths: false });
+      datepicker.show();
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      $febFirst.click();
+      const selectedDate = datepicker.selectedDate;
+      const arr = [
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        selectedDate.getDate(),
+      ];
+      expect(arr.join('-')).toBe('2024-1-1');
+    });
+  });
+
+  describe('options.selectedDate', () => {
+    it('should select date on init', async () => {
+      const date = new Date('2024-01-01');
+      create($el, { selectedDate: date });
+
+      const inputDate = new Date(datepicker.$input.value);
+      expect(datepicker.selectedDate).not.toBe(null);
+      expect(date.getFullYear()).toBe(inputDate.getFullYear());
+      expect(date.getMonth()).toBe(inputDate.getMonth());
+      expect(date.getDate()).toBe(inputDate.getDate());
+    });
+  });
+
+  describe('options.showOtherMonths', () => {
+    const date = new Date('2024-01-01');
+
+    it(`other months's day should be visible when pass options true`, () => {
+      create($el, { selectedDate: date, showOtherMonths: true });
+      datepicker.show();
+
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      expect($febFirst).toBeVisible();
+    });
+
+    it(`other months's day should be hidden when pass options false`, () => {
+      create($el, { selectedDate: date, showOtherMonths: false });
+      datepicker.show();
+
+      const $febFirst = datepicker.$datepicker.querySelector(
+        '[data-monthindex="1"][data-day="1"]'
+      );
+      expect($febFirst.classList.contains('--hidden')).toBe(true);
+    });
+  });
+
+  describe('options.size', () => {
+    it('input should be small size, when options.size is `small`', () => {
+      create($el, { size: 'small' });
+      expect(datepicker.$target.classList.contains('--small')).toBe(true);
+    });
+  });
+
+  describe('options.theme', () => {
+    it('input should be dark theme, when options.theme is `dark`', () => {
+      create($el, { theme: 'dark' });
+      expect(datepicker.$target.classList.contains('--dark')).toBe(true);
+    });
+  });
+
+  describe('options.titleFormat', () => {
+    const selectedDate = new Date('2024-01-01');
+    const findTitle = () =>
+      datepicker.$datepicker.querySelector('.' + cn('controlsTitle'));
+    it('years', () => {
+      create($el, {
+        animation: false,
+        selectedDate,
+        view: 'years',
+        titleFormat: { years: 'YYYY1 ~~ YYYY2' },
+      });
+
+      datepicker.show();
+
+      const $title = findTitle();
+      expect($title.textContent.includes('2020 ~~ 2029'));
+    });
+
+    it('months', () => {
+      create($el, {
+        animation: false,
+        selectedDate,
+        view: 'months',
+        titleFormat: { months: 'YYYY !!' },
+      });
+
+      datepicker.show();
+
+      const $title = findTitle();
+      expect($title.textContent.includes('2024 !!'));
+    });
+
+    it('days', () => {
+      create($el, {
+        animation: false,
+        selectedDate,
+        view: 'days',
+        titleFormat: { days: 'YYYY-MM !!' },
+      });
+
+      datepicker.show();
+
+      const $title = findTitle();
+      expect($title.textContent.includes('2024-01 !!'));
+    });
+  });
+
+  describe('options.toggleSelected', () => {
+    it('should be unselected when click selected item', () => {
+      create($el, {
+        selectedDate: new Date('2024-01-01'),
+        toggleSelected: true,
+      });
+      datepicker.show();
+      const $day = datepicker.$datepicker.querySelector(
+        '[data-monthindex="0"][data-day="1"]'
+      );
+      $day.click();
+      expect(datepicker.selectedDate).toBe(null);
+    });
+  });
+
+  describe('options.view', () => {
+    const date = new Date('2024-01-01');
+    const cases = [
+      { view: 'days', contains: 'January' },
+      { view: 'months', contains: '2024' },
+      { view: 'years', contains: '2020 - 2029' },
+    ];
+    cases.forEach(({ view, contains }) =>
+      it(`should ${view} view`, () => {
+        create($el, { view, selectedDate: date });
+        const $title = datepicker.$datepicker.querySelector(
+          '.' + cn('controlsTitle')
+        );
+        expect($title.textContent.includes(contains)).toBe(true);
+      })
+    );
+  });
+
+  describe('options.minView', () => {
+    it('view should not be under minView', () => {
+      create($el, { minView: 'months' });
+      datepicker.setCurrentView('days');
+
+      expect(datepicker.currentView).toBe('months');
     });
   });
 });
---
+++ tests/shortcuts.test.js
@@ -0,0 +1,153 @@
+import SnowDatePicker from '@/index';
+
+let $el;
+/** @type {import('../src/index').default} */
+let datepicker;
+
+const opts = {
+  shortcuts: true,
+  animation: false,
+  selectedDate: new Date('2015-07-15'),
+};
+
+/**
+ * @param {import('../types/options').Options} [options]
+ */
+function create(options = {}) {
+  datepicker = new SnowDatePicker($el, { ...opts, ...options });
+}
+
+/**
+ * @param {string} key
+ * @param {{ctrlKey?:boolean; altKey?:boolean; shiftKey?:boolean}} options
+ */
+function triggerKeyboardEvent(key, options) {
+  const event = new KeyboardEvent('keydown', {
+    bubbles: true,
+    cancelable: true,
+    key,
+    ...options,
+  });
+  datepicker.$datepicker.dispatchEvent(event);
+}
+
+beforeEach(() => {
+  document.body.innerHTML = '';
+  datepicker = null;
+  $el = document.createElement('input');
+  document.body.appendChild($el);
+});
+
+describe('SHORTCUTS TEST', () => {
+  it('should be hide when press `Escape`', () => {
+    create();
+    datepicker.show();
+    expect(datepicker.isShow()).toBe(true);
+
+    triggerKeyboardEvent('Escape');
+    expect(datepicker.isShow()).toBe(false);
+  });
+
+  it('should be move focus cell when press `ArrowKey`', () => {
+    create();
+    datepicker.show();
+
+    let day = datepicker.selectedDate.getDate();
+
+    const exec = (key, adder) => {
+      triggerKeyboardEvent(key), (day += adder);
+      expect(datepicker.focusDate.getDate()).toBe(day);
+    };
+
+    exec('ArrowLeft', -1);
+    exec('ArrowRight', 1);
+    exec('ArrowUp', -7);
+    exec('ArrowDown', 7);
+  });
+
+  it('should be select focused cell date, when press `Enter`', () => {
+    create();
+    datepicker.show();
+
+    triggerKeyboardEvent('ArrowRight');
+    const focusDate = datepicker.focusDate;
+    triggerKeyboardEvent('Enter');
+    const { selectedDate, converter } = datepicker;
+    expect(converter.date(selectedDate)).toBe(converter.date(focusDate));
+  });
+
+  it('should be change view, when press `PageUp` or `PageDown`', () => {
+    create();
+    datepicker.show();
+
+    triggerKeyboardEvent('PageUp');
+    expect(datepicker.currentView).toBe('months');
+
+    triggerKeyboardEvent('PageDown');
+    expect(datepicker.currentView).toBe('days');
+  });
+
+  it('should be change month, when press `ArrowKey` with `ctrlKey`', () => {
+    create();
+    datepicker.show();
+
+    let month = datepicker.selectedDate.getMonth();
+    const exec = (key, adder) => {
+      triggerKeyboardEvent(key, { ctrlKey: true });
+      month += adder;
+      expect(datepicker.focusDate.getMonth()).toBe(month);
+    };
+
+    exec('ArrowLeft', -1);
+    exec('ArrowDown', -1);
+    exec('ArrowRight', 1);
+    exec('ArrowUp', 1);
+  });
+
+  it('should be change year, when press `ArrowKey` with `shiftKey`', () => {
+    create();
+    datepicker.show();
+
+    let year = datepicker.selectedDate.getFullYear();
+    const exec = (key, adder) => {
+      triggerKeyboardEvent(key, { shiftKey: true });
+      year += adder;
+      expect(datepicker.focusDate.getFullYear()).toBe(year);
+    };
+
+    exec('ArrowLeft', -1);
+    exec('ArrowDown', -1);
+    exec('ArrowRight', 1);
+    exec('ArrowUp', 1);
+  });
+
+  it('should be change year, when press `ArrowKey` with `altKey`', () => {
+    create();
+    datepicker.show();
+
+    let year = datepicker.selectedDate.getFullYear();
+    const exec = (key, adder) => {
+      triggerKeyboardEvent(key, { altKey: true });
+      year += adder;
+      expect(datepicker.focusDate.getFullYear()).toBe(year);
+    };
+
+    exec('ArrowLeft', -10);
+    exec('ArrowDown', -10);
+    exec('ArrowRight', 10);
+    exec('ArrowUp', 10);
+  });
+
+  it('should be change viewDate, when move ohter month', () => {
+    create({ selectedDate: new Date('2024-01-01') });
+    datepicker.show();
+
+    triggerKeyboardEvent('ArrowLeft');
+    const formatDate = datepicker.converter.format(
+      datepicker.focusDate,
+      'YYYY-MM-DD'
+    );
+    expect(formatDate).toBe('2023-12-31');
+    expect(datepicker.viewDate.getMonth()).toBe(11);
+  });
+});
